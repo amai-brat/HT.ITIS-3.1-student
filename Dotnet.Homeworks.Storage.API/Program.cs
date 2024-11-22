1981e0ef7087c45ea230833c70cdc5835146fb3f
Almaz Shamilov <pepegapeepoclown4head@gmail.com>
2024-11-23 02:11:39 +0300

    выполнил hw8

--- Dotnet.Homeworks.Storage.API/Program.cs
+++ Dotnet.Homeworks.Storage.API/Program.cs
@@ -1,10 +1,12 @@
 using Dotnet.Homeworks.Storage.API.Configuration;
 using Dotnet.Homeworks.Storage.API.Endpoints;
 using Dotnet.Homeworks.Storage.API.Services;
+using Dotnet.Homeworks.Storage.API.ServicesExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<MinioConfig>(builder.Configuration.GetSection("MinioConfig"));
+builder.Services.AddMinioClient();
 builder.Services.AddSingleton<IStorageFactory, StorageFactory>();
 builder.Services.AddHostedService<PendingObjectsProcessor>();
 
--- Dotnet.Homeworks.Storage.API/ServicesExtensions/AddMinioExtensions.cs
+++ Dotnet.Homeworks.Storage.API/ServicesExtensions/AddMinioExtensions.cs
@@ -1,12 +1,36 @@
 using Dotnet.Homeworks.Storage.API.Configuration;
+using Microsoft.Extensions.Options;
+using Minio;
 
 namespace Dotnet.Homeworks.Storage.API.ServicesExtensions;
 
 public static class AddMinioExtensions
 {
     public static IServiceCollection AddMinioClient(this IServiceCollection services,
-        MinioConfig minioConfiguration)
+        MinioConfig? conf = default)
     {
-        throw new NotImplementedException();
+        if (conf == null)
+        {
+            services.AddSingleton<IMinioClient, MinioClient>(sp =>
+            {
+                conf = sp.GetService<IOptionsMonitor<MinioConfig>>()!.CurrentValue;
+                return GetMinioClient(conf);
+            });
+        }
+        else
+        {
+            services.AddSingleton<IMinioClient, MinioClient>(_ => GetMinioClient(conf));
+        }
+
+        return services;
+    }
+
+    private static MinioClient GetMinioClient(MinioConfig conf)
+    {
+        return new MinioClient()
+            .WithCredentials(conf.Username, conf.Password)
+            .WithEndpoint(conf.Endpoint, conf.Port)
+            .WithSSL(conf.WithSsl)
+            .Build();
     }
 }
